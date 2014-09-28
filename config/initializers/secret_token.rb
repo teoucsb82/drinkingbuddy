2cf13bbbf29e79e8ff9aca3af89595376121ca0d
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-09-27 23:57:36 -0700

    add all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drinkingbuddy::Application.config.secret_key_base = '8b06b0340e3a0aecba6cc2bac5cc448eb3b2af51de1fe1525cc67c7fd337d3679b0743e79b6a825b494159f374bc9a39154d42b052b31ab411173a49334b314a'
