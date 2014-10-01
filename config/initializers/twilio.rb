0b0de5de66f628730ca69c44ab00c03dfdc2c409
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-09-30 23:44:55 -0700

    take 3

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -8,7 +8,7 @@ if Rails.env.development?
   end
 else
   Twilio.configure do |config|
-    config.account_sid = TWILIO_ACCOUNT_SID
-    config.auth_token = TWILIO_AUTH_TOKEN
+    config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+    config.auth_token = ENV['TWILIO_AUTH_TOKEN']
   end
 end
